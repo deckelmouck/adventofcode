11b03a35efbc0d51e4dddcd76feb3d14c38a6979
deckelmouck <nikolaspalm@gmail.com>
2020-12-16 10:00:08 +0100

    solved 2020 day 15 part 2

--- 2020/Day15/solution.cs
+++ 2020/Day15/solution.cs
@@ -15,59 +15,91 @@ namespace adventofcode
         {
             var input = "11,18,0,20,1,7,16"; //"0,3,6";
 
-            List<int[]> numbers = new List<int[]>();
-
-            foreach (var item in input.Split(","))
+            if(part == 1)
             {
-                int[] number = new int[2];
-                number[0] = Convert.ToInt32(item);
-                number[1] = 0;
-                numbers.Add(number);   
-            }
+                List<int[]> numbers = new List<int[]>();
 
-            List<Spoken> spokenList = new List<Spoken>();
-            int searchTurn = (part == 1) ? 2020 : 30000000;
-
-            for (int i = 0; i < searchTurn; i++)
-            {
-                int turn = i + 1;
-                Console.Write(string.Format("Turn {0} - ", (i+1).ToString()));
-
-                if(i < numbers.Count)
+                foreach (var item in input.Split(","))
                 {
-                    int[] spoken = new int[2];
-                    spoken = numbers[i];
-                    spokenList.Add(new Spoken {Turn = turn, SpokenNumber = spoken[0]});
-                    Console.WriteLine(string.Format("Spoken {0}", spoken[0].ToString()));
+                    int[] number = new int[2];
+                    number[0] = Convert.ToInt32(item);
+                    number[1] = 0;
+                    numbers.Add(number);   
                 }
-                else if (i == 3)
-                {
-                    spokenList.Add(new Spoken {Turn = turn, SpokenNumber = 0});
-                    Console.WriteLine(string.Format("Spoken 0"));
-                }
-                else
-                {
-                    int lastturn = i;
-                    int lastspoken = spokenList.Find(x => x.Turn == lastturn).SpokenNumber;
 
-                    int inspoken = spokenList.FindAll(x => x.SpokenNumber == lastspoken && x.Turn < turn - 1).Count;
+                List<Spoken> spokenList = new List<Spoken>();
+                int searchTurn = (part == 1) ? 2020 : 30000000;
 
-                    if(inspoken == 0)
+                for (int i = 0; i < searchTurn; i++)
+                {
+                    int turn = i + 1;
+                    Console.Write(string.Format("Turn {0} - ", (i+1).ToString()));
+
+                    if(i < numbers.Count)
+                    {
+                        int[] spoken = new int[2];
+                        spoken = numbers[i];
+                        spokenList.Add(new Spoken {Turn = turn, SpokenNumber = spoken[0]});
+                        Console.WriteLine(string.Format("Spoken {0}", spoken[0].ToString()));
+                    }
+                    else if (i == 3)
                     {
                         spokenList.Add(new Spoken {Turn = turn, SpokenNumber = 0});
                         Console.WriteLine(string.Format("Spoken 0"));
                     }
-                    else 
+                    else
                     {
-                        var lastturninlist = spokenList.FindLast(x => x.SpokenNumber == lastspoken && x.Turn < turn - 1).Turn;
-                        int newnumber = turn - lastturninlist - 1;
+                        int lastturn = i;
+                        int lastspoken = spokenList.Find(x => x.Turn == lastturn).SpokenNumber;
 
-                        spokenList.Add(new Spoken {Turn = turn, SpokenNumber = newnumber});
-                        Console.WriteLine(string.Format("Spoken {0}", newnumber));
+                        int inspoken = spokenList.FindAll(x => x.SpokenNumber == lastspoken && x.Turn < turn - 1).Count;
+
+                        if(inspoken == 0)
+                        {
+                            spokenList.Add(new Spoken {Turn = turn, SpokenNumber = 0});
+                            Console.WriteLine(string.Format("Spoken 0"));
+                        }
+                        else 
+                        {
+                            var lastturninlist = spokenList.FindLast(x => x.SpokenNumber == lastspoken && x.Turn < turn - 1).Turn;
+                            int newnumber = turn - lastturninlist - 1;
+
+                            spokenList.Add(new Spoken {Turn = turn, SpokenNumber = newnumber});
+                            Console.WriteLine(string.Format("Spoken {0}", newnumber));
+                        }
                     }
                 }
+
             }
-            
+            else if (part == 2)
+            {
+                Dictionary<int, int> spokenDic = new Dictionary<int, int>();
+
+                for (int i = 0; i < input.Split(',').Length; i++)
+                {
+                    spokenDic.Add(Convert.ToInt32(input.Split(',')[i]), i + 1);
+                }
+
+                int lastnumber = Convert.ToInt32(input.Split(',')[input.Split(',').Length - 1]);
+                int searchTurn = 30000000;
+
+                for (int i = spokenDic.Count + 1; i <= searchTurn; i++)
+                {
+                    if(spokenDic.ContainsKey(lastnumber))
+                    {
+                        int spoken = i-1 - spokenDic[lastnumber];
+                        spokenDic[lastnumber] = i-1;
+                        lastnumber = spoken;
+                    }
+                    else
+                    {
+                        spokenDic[lastnumber] = i - 1;
+                        lastnumber = 0;
+                    }
+                }
+
+                Console.WriteLine(string.Format("last number on turn {0} is: {1}", searchTurn, lastnumber));
+            }    
         }
     }
 }
