bc8e9dc1d12397d4a799d0e63c17400bca44b8ce
deckelmouck <nikolaspalm@gmail.com>
2023-12-16 14:07:34 +0100

    add day16

---
+++ 2023/Day16/solution.cs
@@ -0,0 +1,17 @@
+using System;
+using adventofcode;
+
+namespace aoc2023;
+
+public class solutionDay16 : ISolver
+{
+    public void SolvePart1()
+    {
+        Console.WriteLine("Day 16, Part 1");
+    }
+
+    public void SolvePart2()
+    {
+        Console.WriteLine("Day 16, Part 2");
+    }
+}
