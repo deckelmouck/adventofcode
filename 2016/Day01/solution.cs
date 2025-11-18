e77f99d4134d3c4f54408878bef6c4697f405b3d
deckelmouck <nikolaspalm@gmail.com>
2025-11-18 12:53:52 +0100

    solved 2016 day 01

--- 2016/Day01/solution.cs
+++ 2016/Day01/solution.cs
@@ -18,19 +18,59 @@ class Solution : BaseSolution, ISolver
         {
             instructions.AddRange(line.Split(", "));
         }
+        
+        int direction = 0;
+
+        List<int[]> moves = new();
+
+        foreach (string path in instructions)
+        {
+            char turn = path[0];
+            int way = int.Parse(path.Remove(0,1));
+
+            if (turn == 'L')
+                direction = direction - 90;
+            else
+                direction = direction + 90;
+
+            int[] move = [direction, way];
+            moves.Add(move);                
+
+
+            Console.WriteLine($"path: {path}, turn: {turn}, way: {way}, direction: {direction}, test: {direction % 360}");  
+        }
 
         int x = 0;
         int y = 0;
-        
-        foreach (string path in instructions)
+
+        foreach (var item in moves)
         {
-            char direction = path[0];
-            int way = int.Parse(path.Remove(0,1));
+            int dir = item[0];
+            int steps = item [1];
 
-            
+            switch (dir%360)
+            {
+                case 0:
+                    y = y + steps;
+                    break;
+                case 90:
+                case -270:
+                    x = x + steps;
+                    break;
+                case 180:
+                case -180:
+                    y = y - steps;
+                    break;
+                case 270:
+                case -90:
+                    x = x - steps;
+                    break;
+                default:
+                    break;
+            }
+            //Console.WriteLine($"x: {x}, y: {y}, manhatten_sum: {int.Abs(x)+int.Abs(y)}");
         }
-
-        Console.WriteLine($"x = {x}, y = {y}.");
+        Console.WriteLine($"x: {x}, y: {y}, manhatten_sum: {int.Abs(x)+int.Abs(y)}");
     }
 
     public void SolvePart2()
