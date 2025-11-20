514d42458126ff9bfb6a93e2fe82f9728341f2db
deckelmouck <nikolaspalm@gmail.com>
2025-11-20 17:59:35 +0100

    add test for 2016 day 01

--- adventofcode.tests/UnitTest1.cs
+++ adventofcode.tests/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿namespace adventofcode.tests;
+﻿using adventofcode.Year2016.Day01;
+
+namespace adventofcode.tests;
 
 public class UnitTest1
 {
@@ -6,5 +8,16 @@ public class UnitTest1
     public void Test1()
     {
 
+    }
+    
+    [Theory]
+    [InlineData("R5, L5, R5, R3", 12)]
+    [InlineData("R2, R2, R2", 2)]
+    [InlineData("R2, L3", 5)]
+    public void Test1601(string input, int expected)
+    {
+        Solution sol = new Solution();
+        int ret = sol.Part1(input);
+        Assert.Equal(ret, expected);
     }
 }
--- adventofcode/2016/Day01/solution.cs
+++ adventofcode/2016/Day01/solution.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace adventofcode.Year2016.Day01;
 
-class Solution : BaseSolution, ISolver
+public class Solution : BaseSolution, ISolver
 {
     public void SolvePart1()
     {
@@ -76,5 +76,68 @@ class Solution : BaseSolution, ISolver
     public void SolvePart2()
     {
         throw new System.NotImplementedException();
+    }
+
+    public int Part1(string args)
+    {
+        //var inputPath = GetInputFilePath();
+        //var input = System.IO.File.ReadAllLines(inputPath);
+        //string[] input = ["R5, L5, R5, R3"]; //12
+        //string[] input = ["R2, R2, R2"]; //2
+        //string[] input = ["R2, L3"]; //5
+        List<string> instructions = [.. args.Split(", ")];
+        
+        int direction = 0;
+
+        List<int[]> moves = new();
+
+        foreach (string path in instructions)
+        {
+            char turn = path[0];
+            int way = int.Parse(path.Remove(0,1));
+
+            if (turn == 'L')
+                direction = direction - 90;
+            else
+                direction = direction + 90;
+
+            int[] move = [direction, way];
+            moves.Add(move);                
+
+
+            //Console.WriteLine($"path: {path}, turn: {turn}, way: {way}, direction: {direction}, test: {direction % 360}");  
+        }
+
+        int x = 0;
+        int y = 0;
+
+        foreach (var item in moves)
+        {
+            int dir = item[0];
+            int steps = item [1];
+
+            switch (dir%360)
+            {
+                case 0:
+                    y = y + steps;
+                    break;
+                case 90:
+                case -270:
+                    x = x + steps;
+                    break;
+                case 180:
+                case -180:
+                    y = y - steps;
+                    break;
+                case 270:
+                case -90:
+                    x = x - steps;
+                    break;
+                default:
+                    break;
+            }
+            //Console.WriteLine($"x: {x}, y: {y}, manhatten_sum: {int.Abs(x)+int.Abs(y)}");
+        }
+        return int.Abs(x)+int.Abs(y);
     }
 }
